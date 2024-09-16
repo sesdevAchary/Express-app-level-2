34e5a38756628269e4b22e6a28a39902d1b165fb
sesdevAchary <sesdev.achary@gmail.com>
2024-09-16 09:53:16 +0000

    project file using mDB

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require ('mongoose');
+
+const connectDB= async()=>{
+
+    try{
+        await mongoose.connect('mongodb;//localhost:27017/mydatabase');
+        console.log('connected')
+    }
+
+    catch(err)
+    {
+        console.err("failed to connect ",err);
+        process.exit(1);
+    }
+};
+module.exports= connectDB;
--- server.js
+++ server.js
@@ -1,90 +1,17 @@
-const express= require('express')
+const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
-const mongoose= require('mongoose')
-const port= 3000;
-//connect to mongodb
-mongoose.connect('mongodb://localhost/myDatabase',{ useNewUrlParser: true,useUnifiedTopology:true });
+const PORT = process.env.PORT || 3000;
 
+// Connect to MongoDB
+connectDB();
 
-const db=mongoose.connection
-db.on('error',console.error
-       .bind(console,'mongodb connectionerror:')
-)
-db.once('open',()=>{
-    console.log("connected to mongodb")
-})
+app.use(express.json());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req,res)=>{
-    res.send(" hello boi");
+app.get("/", (req, res) => {
+    res.send("HomePage Of The App");
 });
-
-let books=[];
-app.use(express.json())
-
-app.post('/books', (req,res)=>{
-    const {title,author}=req.body;
-    if (!title || !author){
-        return res.statusCode(400).send('missing')
-    }
-    const newBook = { id: books.length + 1, title, author };
-  books.push(newBook);
-  res.status(201).send(newBook);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
-    
-
-
-app.get('/books', (req,res)=>{
-    res.send("get  request path is working")
-})
-
-app.get('/books/:id', (req,res)=>{
-    const book = books.find(b=>b.id === parseInt(req.params.id));
-    if (!book) {
-        return res.send("book not found")
-        }
-        res.json(book)
-})
-
-app.put('/books/:id', (req,res)=>{
-
-    const book = books.find(b=>b.id===parseInt(req.params.id));
-    if (!book) {
-        return res.status(404).send("book not found")
-    }
-    const {title,author} = req.body
-    book.title = title|| book.title;
-    book.author = author || book.author;
-
-    res.send(" put by ID request path is working")
-})
-app.delete('/books/:id', (req,res)=>{
-    const bookIndex=books.findIndex(b=>b.id === parseInt(req.params.id))
-        if (bookIndex === -1) {
-            return res.status(404).send("book not foud");
-    }
-    book.splice(bookIndex,1) // to delete//
-    res.status(204).send // successfully deleted
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
